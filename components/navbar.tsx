ae00be456c5b977582ca6005bc37bc84336fbb85
Milind Dhamu <milind.dhamu.111@gmail.com>
2024-01-04 11:59:31 +0530

    New: Added Vercel's Speed Insights component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
 		<NextUINavbar maxWidth="lg" position="sticky" shouldHideOnScroll isBordered>
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 			<NavbarBrand as="li" className="gap-3 max-w-fit">
-					<NextLink className="flex justify-start items-center gap-1" href="/">
+					<NextLink aria-label="Logo link" className="flex justify-start items-center gap-1" href="/">
 						<Logo />
 					</NextLink>
 				</NavbarBrand>
