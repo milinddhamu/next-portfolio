71bdc61ca9d47b7cae84181b2bbb644c8b0aa096
Milind Dhamu <milind.dhamu.111@gmail.com>
2024-04-18 16:22:07 +0530

    [Bug]: getLocaleString had issue at server build ,replaced it with date obj.

--- components/blog-cards.tsx
+++ components/blog-cards.tsx
@@ -22,15 +22,16 @@ export const BlogCard: React.FC<BlogCardProps> = ({data}) => {
   };
   const fromNow = (date: number) => {
     const dateObj = new Date(date);
-    return moment(dateObj.toLocaleDateString(), "DD/MM/YYYY").fromNow();
+    const ago = moment(dateObj).fromNow();
+    return ago;
   };
   const time = fromNow(data?.published);
   return (
     <>
     <Card isFooterBlurred style={{borderRadius:"22px"}} className="h-[300px] border-1 border-gray-500/50 w-full">
-      <CardHeader className="absolute z-10 flex-col items-start gap-2">
+      {/* <CardHeader className="absolute z-10 flex-col items-start gap-2">
         <p className="text-xs text-white/80 font-bold">{data?.title}</p>
-      </CardHeader>
+      </CardHeader> */}
       <Image
         loading="lazy"
         removeWrapper
@@ -38,23 +39,25 @@ export const BlogCard: React.FC<BlogCardProps> = ({data}) => {
         className="z-0 w-full h-full object-cover brightness-50"
         src={imageUrl}
       />
-      <span className="flex gap-1 absolute bottom-16 p-1 w-full">
-        {Tags?.map((cat:string)=><Chip classNames={{content:"text-tiny"}} radius="sm" key={cat} variant="flat" size="sm">{cat}</Chip>)}
+      <span className="flex gap-1 absolute bottom-14 p-1 w-full overflow-scroll scrollbar-hide">
+        {Tags?.map((cat:string)=><Chip classNames={{content:"text-tiny whitespace-nowrap"}} radius="sm" key={cat} variant="flat" size="sm">{cat}</Chip>)}
       </span>
      
       <CardFooter style={{borderRadius:"18px",border:"0px"}} className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden p-1  absolute bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
         <div className="flex flex-grow gap-2 items-center">
         <Link isExternal href="https://medium.com/@milind.dhamu.123">
-          <Image
-            alt="Breathing app icon"
-            loading="lazy"
-            style={{borderRadius:"16px"}}
-            className=" w-10 h-10 bg-black p-2"
-            src="https://emojiisland.com/cdn/shop/products/Smiling_Devil_Emoji_large.png"
-          />
+          <div className="w-10 h-10">
+            <Image
+              alt="Breathing app icon"
+              loading="lazy"
+              style={{borderRadius:"16px"}}
+              className="bg-black p-2"
+              src="https://emojiisland.com/cdn/shop/products/Smiling_Devil_Emoji_large.png"
+            />
+          </div>
           </Link>
-          <div className="flex flex-col">
-            <p className="text-tiny text-white/60 font-bold">{data?.author}</p>
+          <div className="flex flex-col max-w-fit">
+            <p className="text-tiny text-white/60 font-bold line-clamp-1">{data?.title}</p>
             <p className="text-tiny text-white/60">Published : {time}</p>
           </div>
         </div>
--- components/reveal-text.tsx
+++ components/reveal-text.tsx
@@ -5,7 +5,7 @@ const RevealSentence = () => {
     <>
       <div className="overflow-hidden w-full">
       <h2 className="text-base opacity-70 text-center">
-      Passionate <p className="font-semibold inline-block bg-gradient-to-t from-violet-900 to-violet-100 bg-clip-text text-transparent ">Full Stack Developer</p> crafting extraordinary projects. Let&apos;s build together!
+      Passionate <p className="animate-text font-semibold inline-block bg-gradient-radial from-violet-300 via-violet-700 to-blue-700 bg-clip-text text-transparent">Full Stack Developer</p> crafting extraordinary projects. Let&apos;s build together!
       </h2>
     </div>
     </>
